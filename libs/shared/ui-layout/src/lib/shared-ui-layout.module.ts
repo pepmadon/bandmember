6f0b249458f3be19d403c11589c9b9cbda959e05
Pep Madon <pepmadon@gmail.com>
2022-10-18 11:53:11 -0500

    added ui-main to  ui-layout

--- libs/shared/ui-layout/src/lib/shared-ui-layout.module.ts
+++ libs/shared/ui-layout/src/lib/shared-ui-layout.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import {SharedUiToolbarModule} from '@bandmember/shared/ui-toolbar';
+import { SharedUiMainModule } from '@bandmember/shared/ui-main';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedUiToolbarModule
+    SharedUiToolbarModule,
+    SharedUiMainModule,
   ],
   declarations: [LayoutComponent],
 })
---
+++ libs/shared/ui-main/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/shared-ui-main.module';
+export * from './lib/main/main.component';
