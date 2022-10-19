2f28cd5acf77189c989aefd7b71cac66dac44c2b
Pep Madon <pepmadon@gmail.com>
2022-10-19 14:05:53 -0500

    added nestjs product and user modules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule} from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, ProductModule],
 
 })
 export class AppModule {}
