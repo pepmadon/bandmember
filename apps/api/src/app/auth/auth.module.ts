4003ede41ec61179a81f6d04b81ebb18732d0ff4
Pep Madon <pepmadon@gmail.com>
2022-10-22 09:24:32 -0500

    passport updated authmod

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
-import {Module} from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module } from '@nestjs/common';
+
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import {JwtModule} from "@nestjs/jwt";
+import { AtStrategy, RtStrategy } from './strategy';
 
 @Module({
-
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AtStrategy, RtStrategy, ],
 })
-
 export class AuthModule {}
