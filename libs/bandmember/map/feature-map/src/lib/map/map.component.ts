080bc35a928246c4c17766c9e2034c94ee0b0398
Pep Madon <pepmadon@gmail.com>
2023-01-07 12:39:20 -0600

    added map component rendering

--- libs/bandmember/map/feature-map/src/lib/map/map.component.ts
+++ libs/bandmember/map/feature-map/src/lib/map/map.component.ts
@@ -1,31 +1,70 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import * as Leaflet from 'leaflet';
+Leaflet.Icon.Default.imagePath = 'assets/';
+
 @Component({
   selector: 'bandmember-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements AfterViewInit {
-  private map!: L.Map | L.LayerGroup<any>;
-
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
+export class MapComponent  {
+  map!: Leaflet.Map;
+  markers: Leaflet.Marker[] = [];
+  options = {
+    layers: [
+      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })
+    ],
+    zoom: 16,
+    center: { lat: 28.626137, lng: 79.821603 }
   }
 
-  
-
-  ngAfterViewInit(): void {
-    this.initMap();
+  initMarkers() {
+    const initialMarkers = [
+      {
+        position: { lat: 28.625485, lng: 79.821091 },
+        draggable: true
+      },
+      {
+        position: { lat: 28.625293, lng: 79.817926 },
+        draggable: false
+      },
+      {
+        position: { lat: 28.625182, lng: 79.81464 },
+        draggable: true
+      }
+    ];
+    for (let index = 0; index < initialMarkers.length; index++) {
+      const data = initialMarkers[index];
+      const marker = this.generateMarker(data, index);
+      marker.addTo(this.map).bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
+      this.map.panTo(data.position);
+      this.markers.push(marker)
+    }
   }
+
+  generateMarker(data: any, index: number) {
+    return Leaflet.marker(data.position, { draggable: data.draggable })
+      .on('click', (event) => this.markerClicked(event, index))
+      .on('dragend', (event) => this.markerDragEnd(event, index));
+  }
+
+  onMapReady($event: Leaflet.Map) {
+    this.map = $event;
+    this.initMarkers();
+  }
+
+  mapClicked($event: any) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerClicked($event: any, index: number) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerDragEnd($event: any, index: number) {
+    console.log($event.target.getLatLng());
+  } 
+
 }
